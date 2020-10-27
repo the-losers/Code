4ac3f116e91044e990d9fe1dd4544db98ae76bce
the-losers <47303464+the-losers@users.noreply.github.com>
2020-10-28 00:20:48 +0530

    Update linkList_Basic.java

--- linkList_Basic.java
+++ linkList_Basic.java
@@ -35,6 +35,22 @@ public class LinkedListUse {
 
 	}
 
+	public static Node<Integer> insert(Node<Integer> head, int data, int pos){
+		Node<Integer> newNode = new Node<Integer>(data);
+		if(pos == 0){
+			newNode.next = head;
+			return newNode;
+		}
+		int i = 0;
+		Node<Integer> temp = head;
+		while(i < pos - 1){
+			temp = temp.next;
+			i++;
+		}
+		newNode.next = temp.next;
+		temp.next = newNode;
+		return head;
+	}
 	public static void main(String[] args) {
 		Node<Integer> node1 = new Node<Integer>(10);
 		Node<Integer> node2 = new Node<Integer>(20);
