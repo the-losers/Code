debb2498326df74cda00d8e6cb30aaa800be4a6e
the-losers <47303464+the-losers@users.noreply.github.com>
2020-11-10 23:23:32 +0530

    Create queue_ll.java

---
+++ queue_ll.java
@@ -0,0 +1,98 @@
+public class QueueUsingLL<T> {
+
+	private Node<T> front;
+	private Node<T> rear;
+	private int size;
+
+	public QueueUsingLL() {
+		front = null;
+		rear = null;
+		size = 0;
+	}
+	int size(){
+		return size;
+	}
+
+	boolean isEmpty(){
+		return size == 0;
+	}
+
+	T front() throws QueueEmptyException{
+		if(size == 0){
+			throw new QueueEmptyException();
+		}	
+		return front.data;
+	}
+
+	void enqueue(T element){
+		Node<T> newNode = new Node<>(element);
+		if(rear == null){
+			front = newNode;
+			rear = newNode;
+		}else{
+			rear.next = newNode;
+			rear = newNode;
+		}
+		size++;
+
+	}
+
+	T dequeue() throws QueueEmptyException{
+		if(size == 0){
+			throw new QueueEmptyException();
+		}	
+		
+		T temp = front.data;
+		front = front.next;
+		if(size == 1){
+			rear = null;
+		}
+		size--;
+		
+		return temp;
+	}
+
+
+}
+
+
+public class Node<T> {
+	
+	T data;
+	Node<T> next;
+	
+	Node(T data){
+		this.data = data;
+		next = null;
+	}
+	
+}
+
+
+public class QueueUse {
+
+	public static void main(String[] args) {
+//		QueueUsingArray queue = new QueueUsingArray(3);
+		QueueUsingLL<Integer> queue = new QueueUsingLL<>();
+		for(int i = 1; i <= 5; i++){
+			queue.enqueue(i);
+//			try {
+//				queue.enqueue(i);
+//			} catch (QueueFullException e) {
+//				
+//			}
+		}
+		
+		
+		while(! queue.isEmpty()){
+			try {
+				System.out.println(queue.dequeue());
+			} catch (QueueEmptyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
